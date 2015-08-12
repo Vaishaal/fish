486efcde07d621bdcf217e0a2559ec10ba136670
Vaishaal Shankar <vaishaal_shankar@apple.com>
2015-08-12 15:14:51 -0700

    initial commit, fish

---
+++ config.fish
@@ -0,0 +1,18 @@
+fish_vi_mode
+fzf_key_bindings
+set -gx PATH $PATH ~rc/bin/
+set -gx PATH $PATH ~osdev/bin
+alias e='emacsclient -t --alternate-editor=""'
+#alias vi='nvim'
+#alias vim='nvim'
+# in .config.fish
+
+function fish_prompt
+  env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.config/fish/shell_prompt.sh left
+end
+
+function fish_right_prompt
+  env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.config/fish/.shell_prompt.sh right
+end
+
+
