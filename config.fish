b2b92529b9f540a3c4ee16142503877d01d75fd3
Vaishaal Shankar <vaishaal@berkeley.edu>
2017-02-25 20:48:35 +0000

    updated config for 2017

--- config.fish
+++ config.fish
@@ -1,10 +1,4 @@
-fish_vi_key_bindings
-set -gx PATH $PATH ~rc/bin/
-set -gx PATH $PATH ~osdev/bin
-alias e='emacsclient -t --alternate-editor=""'
-#alias vi='nvim'
-#alias vim='nvim'
-# in .config.fish
+fish_vi_mode
 
 function fish_prompt
   env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.config/fish/.shell_prompt.sh left
@@ -13,5 +7,10 @@ end
 function fish_right_prompt
   env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.config/fish/.shell_prompt.sh right
 end
+
+set -gx PATH $HOME/anaconda3/bin $PATH
+alias vim nvim
+
+
 
 
