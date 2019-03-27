a83ba1787981260135b1c165528c06b360dade0f
Vaishaal Shankar <vaishaal@berkeley.edu>
2019-03-27 13:50:46 -0700

    more fish stuff

--- config.fish
+++ config.fish
@@ -1,4 +1,4 @@
-fish_vi_mode
+fish_vi_key_bindings
 
 function fish_prompt
   env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.config/fish/.shell_prompt.sh left
@@ -10,6 +10,15 @@ end
 
 set -gx PATH $HOME/anaconda3/bin $PATH
 set -gx EDITOR vim
+
+alias a='fasd -a'        # any
+alias s='fasd -si'       # show / search / select
+alias d='fasd -d'        # directory
+alias f='fasd -f'        # file
+alias sd='fasd -sid'     # interactive directory selection
+alias sf='fasd -sif'     # interactive file selection
+alias z='fasd_cd -d'     # cd, same functionality as j in autojump
+alias zz='fasd_cd -d -i' # cd with interactive selection
 
 function ec
     set -q EDITOR; or return 1
